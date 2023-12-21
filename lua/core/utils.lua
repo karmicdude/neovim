076ba6355e84eb305176b814096d3487230d7be5
KarmicDude <voronov.semyon@gmail.com>
2023-12-22 02:50:44 +0900

    feat: add helper functions

--- lua/core/utils.lua
+++ lua/core/utils.lua
@@ -7,4 +7,44 @@ M.autocmd_group = function(name, clear)
 	return vim.api.nvim_create_augroup("user_" .. name, { clear = clear })
 end
 
+-- pick window for focus
+M.pick_window = function()
+	local window_picker = require("window-picker")
+	local picked_window_id = window_picker.pick_window({
+		include_current_win = true,
+	}) or vim.api.nvim_get_current_win()
+	vim.api.nvim_set_current_win(picked_window_id)
+end
+
+--@param cwd string CWD path to open
+--@param win_type string Type of window
+M.ext_terminal = function(cwd, win_type)
+	local kitten = "/Applications/kitty.app/Contents/MacOS/kitten"
+	local socket = "unix:/tmp/kitty"
+	cwd = cwd or vim.fn.getcwd()
+	if cwd == "buffer" then
+		cwd = vim.fn.fnamemodify(vim.fn.expand("%:p"), ":h")
+	end
+	win_type = win_type or "tab"
+	local cmd = string.format(
+		"%s @ --to %s launch --type=%s --cwd='%s' --tab-title='nvim-cmd' --title='nvim-cmd'",
+		kitten,
+		socket,
+		win_type,
+		cwd
+	)
+	vim.fn.jobstart(cmd)
+end
+
+-- rename tab
+M.rename_tab = function()
+	vim.ui.input({ prompt = "Enter a new tab name: " }, function(name)
+		if name == "" then
+			vim.notify("Tab name is required", vim.log.levels.ERROR)
+			return
+		end
+		vim.cmd("TabRename " .. name)
+	end)
+end
+
 return M
--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -1,47 +1,23 @@
 return {
 	"folke/flash.nvim",
 	event = "VeryLazy",
-	opts = {},
+	opts = {
+		search = {
+			exclude = {
+				"notify",
+				"cmp_menu",
+				"noice",
+				"flash_prompt",
+				"NeogitStatus",
+			},
+		},
+	},
+  -- stylua: ignore
 	keys = {
-		{
-			"s",
-			mode = { "n", "x", "o" },
-			function()
-				require("flash").jump()
-			end,
-			desc = "Flash",
-		},
-		{
-			"S",
-			mode = { "n", "x", "o" },
-			function()
-				require("flash").treesitter()
-			end,
-			desc = "Flash Treesitter",
-		},
-		{
-			"r",
-			mode = "o",
-			function()
-				require("flash").remote()
-			end,
-			desc = "Remote Flash",
-		},
-		{
-			"R",
-			mode = { "o", "x" },
-			function()
-				require("flash").treesitter_search()
-			end,
-			desc = "Treesitter Search",
-		},
-		{
-			"<c-s>",
-			mode = { "c" },
-			function()
-				require("flash").toggle()
-			end,
-			desc = "Toggle Flash Search",
-		},
+		{"s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash",},
+		{"S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter",},
+		{"r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash",},
+		{"R", mode = { "o", "x" }, function() require("flash").treesitter_search( ) end, desc = "Treesitter Search",},
+		{"<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search",},
 	},
 }
