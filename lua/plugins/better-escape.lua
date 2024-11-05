6e5f62d829cf1c0a702b2d5fe01506899ba52854
KarmicDude <voronov.semyon@gmail.com>
2024-11-06 00:51:58 +0900

    fix: fix `better-escape` config

--- lua/plugins/better-escape.lua
+++ lua/plugins/better-escape.lua
@@ -2,7 +2,19 @@ return {
 	"max397574/better-escape.nvim",
 	event = "VeryLazy",
 	opts = {
-		mapping = { "jk", "jj", "jf" },
 		timeout = 400,
+		default_mappings = false,
+		mappings = {
+			i = {
+				j = {
+					k = "<Esc>",
+					j = "<Esc>",
+					f = "<Esc>",
+				},
+			},
+			v = {
+				j = {},
+			},
+		},
 	},
 }
