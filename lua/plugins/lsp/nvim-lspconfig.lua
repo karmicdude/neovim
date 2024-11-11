f60b23db0b8f9726987d01b155213e4611b905a4
KarmicDude <voronov.semyon@gmail.com>
2024-11-12 03:59:52 +0900

    chore: add `eslint` configuration

--- lua/plugins/lsp/nvim-lspconfig.lua
+++ lua/plugins/lsp/nvim-lspconfig.lua
@@ -57,6 +57,43 @@ local config = function()
 	lspconfig.eslint.setup({
 		capabilities = caps,
 		on_attach = on_attach,
+		root_dir = function(fname)
+			return lspconfig.util.root_pattern({
+				".vimroot",
+			})(fname)
+		end,
+		settings = {
+			codeAction = {
+				disableRuleComment = {
+					enable = true,
+					location = "separateLine",
+				},
+				showDocumentation = {
+					enable = true,
+				},
+			},
+			codeActionOnSave = {
+				enable = false,
+				mode = "all",
+			},
+			experimental = {
+				useFlatConfig = false,
+			},
+			format = true,
+			nodePath = "",
+			onIgnoredFiles = "off",
+			problems = {
+				shortenToSingleLine = false,
+			},
+			quiet = false,
+			rulesCustomizations = {},
+			run = "onType",
+			useESLintClass = false,
+			validate = "on",
+			workingDirectory = {
+				mode = "location",
+			},
+		},
 	})
 
 	-- Ansilbe
