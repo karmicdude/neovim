463afac5ef65df8deb95ac05d40ad3ee5a21d942
KarmicDude <voronov.semyon@gmail.com>
2026-04-08 00:30:17 +0900

    fix: disable formating `ansible-vault` encrypted files

--- lua/plugins/lsp/conform.lua
+++ lua/plugins/lsp/conform.lua
@@ -31,6 +31,14 @@ return {
 			["hcl"] = { "packer_fmt" },
 		},
 		format_on_save = function(bufnr)
+			-- Exclude ansible-vault encrypted files
+			local ft = vim.bo[bufnr].filetype
+			if ft == "yaml" then
+				local first_line = vim.api.nvim_buf_get_lines(bufnr, 0, 1, false)[1] or ""
+				if first_line:match("^%$ANSIBLE_VAULT") then
+					return nil
+				end
+			end
 			local timeout = vim.b[bufnr].format_timeout or 800
 			return {
 				timeout_ms = timeout,
